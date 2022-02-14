b88d4affc1d73ee7a7932648fb32187898139bff
zb09 <zbdigital009@gmail.com>
2022-02-14 17:08:30 +0800

    modulke

--- src/store/data/index.ts
+++ src/store/data/index.ts
@@ -1,9 +1,10 @@
-import { state } from './state';
+import { Module } from "vuex"
+import { state, TState } from './state';
 import { mutations } from './mutations';
 import { actions } from './actions';
 import { getters } from './getters';
 
-const DataModule = {
+const DataModule: Module<TState, TState> = {
     namespaced: true,
     state,
     mutations,
